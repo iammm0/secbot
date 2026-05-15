9a3e4fb1b22a2456916f6149a4cf45788b71ad5d
Mingjun Zhao <145631324+iammm0@users.noreply.github.com>
2026-05-15 17:09:41 +0800

    feat(tools): add install_tool for auto-installing missing security tools

--- server/src/modules/tools/control/index.ts
+++ server/src/modules/tools/control/index.ts
@@ -1,8 +1,10 @@
 export { ExecuteCommandTool } from './execute-command.tool';
 export { TerminalSessionTool } from './terminal-session.tool';
+export { InstallToolTool } from './install-tool.tool';
 
 import { BaseTool } from '../core/base-tool';
 import { ExecuteCommandTool } from './execute-command.tool';
 import { TerminalSessionTool } from './terminal-session.tool';
+import { InstallToolTool } from './install-tool.tool';
 
-export const CONTROL_TOOLS: BaseTool[] = [new ExecuteCommandTool(), new TerminalSessionTool()];
+export const CONTROL_TOOLS: BaseTool[] = [new ExecuteCommandTool(), new TerminalSessionTool(), new InstallToolTool()];
